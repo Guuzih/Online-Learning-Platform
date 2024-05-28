5b7587ee1f5d40bbd3951ec20b522c44c49b2264
Gustavo Coelho <gustavo.coelh21@gmail.com>
2024-05-28 15:35:23 -0300

    atualizando

---
+++ dist/webpack.config.js
@@ -0,0 +1,30 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = __importDefault(require("path"));
+const webpack_node_externals_1 = __importDefault(require("webpack-node-externals"));
+const config = {
+    entry: './src/server.ts',
+    output: {
+        path: path_1.default.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    externals: [
+        (0, webpack_node_externals_1.default)(),
+    ],
+};
+exports.default = config;
